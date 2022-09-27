62208efc9ef4a32306e855660316ac57dd11e8d3
FTrain27 <thewizard2742@protonmail.com>
2022-09-27 08:58:14 -0400

    Updated module structure & high lvl ops in readme

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Royalty-Smart-Contract
 Plutus-based Cardano Smart Contract for transactions in the Cardano ecosystem.
+What it does (chronological):
+1. Receive any adresses and brand partner royalty %s from server
+2. Check if user NFTs were provided
+3a. If yes, find policy script -> unnamed royalty token w/777 tag -> fetch royalty % due to creators
+3b. If no, carry on
+4. Calcualte royalties due for each item
+5. Construct Tx w/ Tx in -> user wallet Tx out -> any NFT Creator addresses + server provided addresses
--- app/On-Chain.hs
+++ app/On-Chain.hs
@@ -3,13 +3,13 @@ module Main where
 import           PlutusTx             (Data (..))
 import qualified PlutusTx
 import           PlutusTx.Prelude     hiding (Semigroup(..), unless)
-import qualified PlutusTx.Builtins   as Builtins
+import qualified PlutusTx.Builtins    as Builtins
 
 import           Ledger               hiding (singleton)
 import           Ledger.Constraints   (TxConstraints)
 import qualified Ledger.Constraints   as Constraints
 import qualified Ledger.Typed.Scripts as Scripts --Plutus.Script.Utils.V1.Scripts
-import           Ledger.Ada          as Ada
+import           Ledger.Ada           as Ada
 
 import           Playground.Contract (printJson, printSchemas, ensureKnownCurrencies, stage, ToSchema)
 import           Playground.TH       (mkKnownCurrencies, mkSchemaDefinitions)
@@ -18,8 +18,8 @@ import           Playground.Types    (KnownCurrency (..))
 import           Plutus.Contract
 
 import           Control.Monad       hiding (fmap)
-import           Data.Aeson           (ToJSON, FromJSON)
-import           GHC.Generics         (Generic)   
+import           Data.Aeson          (ToJSON, FromJSON)
+import           GHC.Generics        (Generic)   
 import           Data.Map            as Map
 import           Data.Text           (Text)
 import           Data.Void           (Void)
@@ -28,10 +28,26 @@ import           Text.Printf         (printf)
 
 {-# OPTIONS_GHC -fno-warn-unused-imports #-}
 
---ON-CHAIN
-{-# INLINABLE  #-}
+{-# INLINABLE redeemer #-}
+redeemer :: String -> Maybe String -> ScriptContext -> Maybe Bool
+redeemer address _ = traceIfFalse "Wrong redeemer!" (isHexDigit address)
+
+data Typed
+instance Scripts.ValidatorTypes Typed where
+    type instance AddressType Typed = String
+
+typedValidator :: Scripts.TypedValidator Typed
+typedValidator = Scripts.mkTypedValidator @Typed 
+                 $$(PlutusTx.compile [|| redeemer ||])
+                 $$(PlutusTx.compile [|| wrap ||])
+    where
+        wrap = Scripts.wrapValidator @String
+
+validator :: Validator
+validator = Scripts.validatorScript typedValidator
+
+scrAddress :: Ledger.Address
+scrAddress = scriptAddress validator
 
 {-# INLINABLE  #-}
-
---OFF-CHAIN
 
